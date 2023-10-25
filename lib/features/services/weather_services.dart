9611dac1daf0a0875061462609ed95635beeb9c5
Tasneem Zaareer <tasneem.zareer@gmail.com>
2023-10-25 18:48:03 +0300

    create weather services like get Current Weather method & base url & api key

--- lib/features/services/weather_services.dart
+++ lib/features/services/weather_services.dart
@@ -1,8 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+import '../home/data/weather_model.dart';
+
 // class weather services to create needed weather methods
 class WeatherServices {
+  final Dio dio;
+  WeatherServices(this.dio);
+  String baseUrl = 'https://api.openweathermap.org/data/2.5';
+  String apiKey = '87d0ece569fee9a09dacd04914c6f3b6';
 
-  //https://api.openweathermap.org/data/2.5/weather?q=London&appid=87d0ece569fee9a09dacd04914c6f3b6
+
+  //
   //get current weather method
+  Future<Weather> getCurrentWeather(String cityName) async{
+    Response response = await dio.get('$baseUrl/weather?q=$cityName&appid=$apiKey');
+    Weather weather = Weather.fromJson(response.data);
+    return weather;
+  }
 
   //get current city
 
